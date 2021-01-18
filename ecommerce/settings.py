44c72ca89bf23628ab75736b90507a53018818c8
riya87911094 <riya87911094@gmail.com>
2021-01-19 03:35:54 +0530

    edit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -125,9 +125,9 @@ STATIC_ROOT = '/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS=[
- #os.path.join(BASE_DIR,'static')
- #]
+STATICFILES_DIRS=[
+ os.path.join(BASE_DIR,'static')
+ ]
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
